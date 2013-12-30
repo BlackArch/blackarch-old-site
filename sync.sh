abaa0fe005e250b511d3966be729799396574068
Evan Teitelman <teitelmanevan@gmail.com>
2013-12-29 20:01:50 -0500

    sync.sh: Exclude some directories.

--- sync.sh
+++ sync.sh
@@ -10,5 +10,6 @@ trap "kill $SSH_AGENT_PID" EXIT
 ssh-add
 
 # Delete manually or something.
-rsync --chmod a+rX,ug+w -avz ./ "$site:/$rdir" --exclude=.git
+rsync --chmod a+rX,ug+w -avz ./ "$site:/$rdir" \
+  --exclude=.git --exclude=git --exclude=common --exclude=generators
 #ssh "$site" '/var/www/blackarch.org/fixperms.sh'
