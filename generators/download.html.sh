47f51cf1b0c9126d9651605f13e3399b4b20777f
Levon 'noptrix' Kayan <noptrix@nullsecurity.net>
2013-12-31 04:22:06 +0100

    added iso pre-links

--- generators/download.html.sh
+++ generators/download.html.sh
@@ -30,24 +30,24 @@ USB stick using a utility like `dd`.
 <b>DVD</b>
 <br />
 <a href="iso/blackarchlinux-wm-dvd-2014.XX.XX-dual.iso">
-blackarchlinux-wm-dvd-2014.XX.XX-dual.iso</a>
+blackarchlinux-wm-dvd-2014.XX.XX-dual.iso</a> - SHA1: XX - Size: 3.9 GB
 <br />
 <a href="iso/blackarchlinux-kde-dvd-2014.XX.XX-dual.iso">
-blackarchlinux-kde-dvd-2014.XX.XX-dual.iso</a>
+blackarchlinux-kde-dvd-2014.XX.XX-dual.iso</a> - SHA1: XX - Size: 3.9 GB
 <br />
 <a href="iso/blackarchlinux-gnome-dvd-2014.XX.XX-dual.iso">
-blackarchlinux-gnome-dvd-2014.XX.XX-dual.iso</a>
+blackarchlinux-gnome-dvd-2014.XX.XX-dual.iso</a> - SHA1: XX - Size: 3.9 GB
 <br /><br />
 <b>CD</b>
 <br />
 <a href="iso/blackarchlinux-wm-cd-2014.XX.XX-dual.iso">
-blackarchlinux-wm-cd-2014.XX.XX-dual.iso</a>
+blackarchlinux-wm-cd-2014.XX.XX-dual.iso</a> - SHA1: XX - Size: 6XX MB
 <br />
 <a href="iso/blackarchlinux-kde-cd-2014.XX.XX-dual.iso">
-blackarchlinux-kde-cd-2014.XX.XX-dual.iso</a>
+blackarchlinux-kde-cd-2014.XX.XX-dual.iso</a> - SHA1: XX - Size: 6XX MB
 <br />
 <a href="iso/blackarchlinux-gnome-cd-2014.XX.XX-dual.iso">
-blackarchlinux-gnome-cd-2014.XX.XX-dual.iso</a>
+blackarchlinux-gnome-cd-2014.XX.XX-dual.iso</a> - SHA1: XX - Size: 6XX MB
 <br /><br />
 <h3>Mirror Sites</h3>
 <hr>
