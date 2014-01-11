629d2910d60fdc7026122d7a05c0005f5c63f121
Evan Teitelman <teitelmanevan@gmail.com>
2014-01-10 19:02:03 -0500

    sync.sh: Exclude more items.

--- sync.sh
+++ sync.sh
@@ -11,5 +11,5 @@ ssh-add
 
 # Delete manually or something.
 rsync --rsh="ssh -A -l ${USER}" --chmod "a+rX,ug+w" -avz ./ "$site:/$rdir" \
-  --exclude={.git,git,common,generators}
+  --exclude={.git,git,common,generators,sync.sh,clean.sh,generate-site.sh,update-tools.sh}
 #ssh "$site" '/var/www/blackarch.org/fixperms.sh'
