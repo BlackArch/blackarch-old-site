7fa5269591719cfdd4fad0c28775bc4ec4ac858f
Evan Teitelman <teitelmanevan@gmail.com>
2014-01-11 07:24:21 -0500

    download: Point to the readme.

--- generators/download.html.sh
+++ generators/download.html.sh
@@ -3,25 +3,12 @@ cat common/header
 
 cat <<\EOF
 <div id="dl_body">
-<h3>Download BlackArch</h3>
+<h3>Install on Existing Arch Machines</h3>
 <hr>
-<p>To make use of the packages in the toolset follow these instructions.</p>
-<br/>
-<p>Append these lines to <code>/etc/pacman.conf</code> to add the BlackArch repository:</p>
-<p id="cfont_tab">[blackarch]</p>
-<p id="cfont_tab">Server = &lt;mirror_site&gt;</p>
-<p id="cfont_tab">SigLevel = Optional TrustAll</p>
-Where &lt;mirror_site&gt; should be a complete URL pointing to the repository.
-Please use one of our official mirrors. See below.
-<br /><br />
-<p>For package signing, run the following to pull in and sign the package signing keys:</p>
-<p id="cfont_tab">$ sudo pacman-key -r 4345771566D76038C7FEB43863EC0ADBEA87E4E3</p>
-<p id="cfont_tab">$ sudo pacman-key --lsign-key 4345771566D76038C7FEB43863EC0ADBEA87E4E3</p>
-<br/>
-Please ensure that all packages are signed with our valid keys, if they are not
-then you are not installing our packages! See
-<a href="about.html">HERE</a> for the valid keys.
-<br/><br/>
+<p>BlackArch can act as an unofficial user repository. See <a
+		href="https://github.com/BlackArch/blackarch/blob/master/docs/README.md#setup">the README</a>
+	on Github for instructions on installing BlackArch in this manner.</p>
+	<br/><br/>
 
 <!-- Uncomment once the ISO is ready -->
 <!--
