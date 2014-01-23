bdb62bf28f6caad3abb8653bb26f8d053e402a89
Levon 'noptrix' Kayan <noptrix@nullsecurity.net>
2014-01-23 18:29:34 +0100

    added 2 new contributors (mirror offering). added 'target=_blank's

--- generators/about.html.sh
+++ generators/about.html.sh
@@ -35,8 +35,8 @@ cat <<EOF
 		<tr>
 			<td>Evan Teitelman</td>
 			<td>paraxor</td>
-			<td>teitelmanevan[at]gmail.com</td>
-			<td><a href="gpg/paraxor.asc">0xEA87E4E3</a></td>
+			<td>teitelmanevan[at]gmail.com </td>
+			<td><a href="gpg/paraxor.asc" target="_blank">0xEA87E4E3</a></td>
 			<td>4345 7715 66D7 6038 C7FE B438 63EC 0ADB EA87 E4E3</td>
 			<td>Founder, Developer, Committer</td>
 		</tr>
@@ -44,7 +44,7 @@ cat <<EOF
 			<td>Levon Kayan</td>
 			<td>noptrix</td>
 			<td>noptrix[at]nullsecurity.net</td>
-			<td><a href="gpg/noptrix.asc">0xDCA45D42</a></td>
+			<td><a href="gpg/noptrix.asc" target="_blank">0xDCA45D42</a></td>
 			<td>250A 573C CA93 01B3 7A34  7860 4D48 E33A DCA4 5D42</td>
 			<td>Developer, Committer</td>
 		</tr>
@@ -52,7 +52,7 @@ cat <<EOF
 			<td>Javier</td>
 			<td>nrz</td>
 			<td>nrz[at]nullsecurity.net</td>
-			<td><a href="gpg/nrz.asc">0x7C03DD86</a></td>
+			<td><a href="gpg/nrz.asc" target="_blank">0x7C03DD86</a></td>
 			<td>D7A7 F999 851D 0AF4 47D1  C292 0A9A 8EC5 7C03 DD86</td>
 			<td>Developer, Commiter</td>
 		</tr>
@@ -60,7 +60,7 @@ cat <<EOF
 			<td>Ellis Kenyo</td>
 			<td>elken</td>
 			<td>elken.tdos[at]gmail.com</td>
-			<td><a href="gpg/elken.asc">0x0F4093D1</a></td>
+			<td><a href="gpg/elken.asc" target="_blank">0x0F4093D1</a></td>
 			<td>A38C A4EB 2A23 27DC 3713  3E22 0C8D AD09 0F40 93D1</td>
 			<td>Committer</td>
 		</tr>
@@ -68,7 +68,7 @@ cat <<EOF
 			<td>Francesco Piccinno</td>
 			<td>nopper</td>
 			<td>stack.box[at]gmail.com</td>
-			<!--<td><a href="gpg/hoyt.asc">0x13714D33</a></td>-->
+			<!--<td><a href="gpg/hoyt.asc" target="_blank">0x13714D33</a></td>-->
 			<!--<td>D7A7 F999 851D 0AF4 47D1  C292 0A9A 8EC5 7C03 DD86</td>-->
 			<td></td>
 			<td></td>
@@ -78,7 +78,7 @@ cat <<EOF
 			<td>Aaron Lewis</td>
 			<td>Hoyt</td>
 			<td>the.warl0ck.1989[at]gmail.com</td>
-			<td><a href="gpg/hoyt.asc">0x13714D33</a></td>
+			<td><a href="gpg/hoyt.asc" target="_blank">0x13714D33</a></td>
 			<td>9F67 391B B770 8FF6 99DC  D92D 87F6 2602 1371 4D33</td>
 			<td>Commiter</td>
 		</tr>
@@ -86,7 +86,7 @@ cat <<EOF
 			<td></td>
 			<td>sudokode</td>
 			<td>sudokode[at]gmail.com</td>
-			<td><a href="gpg/sudokode.asc">0x8B0554C7</a></td>
+			<td><a href="gpg/sudokode.asc" target="_blank">0x8B0554C7</a></td>
 			<td>54EB C502 AF40 9514 1E00  4E95 9753 5EC1 8B05 54C7</td>
 			<td>Committer</td>
 		</tr>
@@ -94,7 +94,7 @@ cat <<EOF
 			<td></td>
 			<td>t60r</td>
 			<td>merkinmaker[at]gmail.com</td>
-			<!--<td><a href="gpg/hoyt.asc">0x13714D33</a></td>-->
+			<!--<td><a href="gpg/hoyt.asc" target="_blank">0x13714D33</a></td>-->
 			<!--<td>D7A7 F999 851D 0AF4 47D1  C292 0A9A 8EC5 7C03 DD86</td>-->
 			<td></td>
 			<td></td>
@@ -104,7 +104,7 @@ cat <<EOF
 			<td>Pierre B.</td>
 			<td>Pi3rrot</td>
 			<td>pierre[at]pi3rrot.net</td>
-			<td><a href="gpg/pi3rrot.asc">0x0B694394</a></td>
+			<td><a href="gpg/pi3rrot.asc" target="_blank">0x0B694394</a></td>
 			<td>ABE2 FA90 9FAB 5532 AFAE  72CC 6594 137F 0B69 4394</td>
 			<td>Contributor</td>
 		</tr>
@@ -112,8 +112,24 @@ cat <<EOF
       <td>Jeremy Marlow</td>
       <td>i3_Arch</td>
       <td>jamarlow410[at]st.nashcc.edu</td>
-      <td><a href="gpg/i3_arch.asc">0xC8173B68</a></td>
+      <td><a href="gpg/i3_arch.asc" target="_blank">0xC8173B68</a></td>
       <td>C854 01A4 1127 037D 540B  EB76 0147 3701 C817 3B68</td>
+      <td>Contributor</td>
+    </tr>
+    <tr>
+      <td>Guy Márquez</td>
+      <td>w4rk3n</td>
+      <td>guy[at]smartdefence.cl</td>
+      <td><a href="gpg/w4rk3n.asc" target="_blank">0x27F4E268</a></td>
+      <td>C69E 3E53 5F82 3695 A180  CAC0 4A56 9373 27F4 E268</td>
+      <td>Contributor</td>
+    </tr>
+    <tr>
+      <td>Felipe Montes</td>
+      <td>GudwaL</td>
+      <td>felipe[at]smartdefence.cl</td>
+      <td><a href="gpg/gudwal.asc" target="_blank">0x9548C879</a></td>
+      <td>B8EF 5522 B09F EF1E 48C1  6955 1A3B C9D4 9548 C879</td>
       <td>Contributor</td>
     </tr>
 	</table>
